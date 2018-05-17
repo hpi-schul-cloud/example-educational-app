ee9951c9d28311406f0092bc7889544dead1850a
dominikglandorf <do.gl@gmx.de>
2018-05-17 15:45:51 +0200

    show depseudo user name

--- client/src/components/welcome.js
+++ client/src/components/welcome.js
@@ -7,9 +7,12 @@ class Welcome extends Component {
   }
   render() {
     const { userId } = this.props.match.params;
+    const userNameURL = 'http://localhost:3100/account/username/';
     return (
       <div>
-        <p>Hallo {userId}</p>
+        <p>Hallo {userId}, so kennen wir dich.</p>
+        <p>Unpseudonymisiert heißt du wohl: <iframe title="username" style={{ height: '26px', width: '180px', border: 'none' }} src={`${userNameURL}${userId}`} /></p>
+        <iframe title="username" style={{ height: '26px', width: '180px', border: 'none' }} src={`${userNameURL}6c4a44bf-e5d4-41ca-80ae-9b68d5cee563`} />
         <p>Viel Spaß beim Lernen!</p>
       </div>
     );
